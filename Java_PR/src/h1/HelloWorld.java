74dec0fdacd8cc1ea186b4f3af8e185b40c01561
jangminwoo <skipper44@naver.com>
2025-06-05 00:46:14 +0900

    맥북 자바 이클립스 test1 -> 인텔리제이는 좀 더 오류 구글링 하기.

---
+++ Java_PR/src/h1/HelloWorld.java
@@ -0,0 +1,10 @@
+package h1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello MacBook Pro"+", "+"hello world");
+;	}
+
+}
