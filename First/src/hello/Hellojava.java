7516894b7bfcf7694c2ce78baef227eb97f99b9d
jangminwoo <skipper44@naver.com>
2025-11-02 00:06:41 +0900

    test 2025/11/01

---
+++ First/src/hello/Hellojava.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		System.out.println("hello, Java");
+		// 주석 처리는 //을 사용하면 된다. System.out.println("hello, Java");
+		/*
+		 * Date: 2025/11/02
+		 * Author: 장민우
+		 * Description: 연결 테스트 업로드 자바 프로그램입니다.
+		 * Version: 1.0
+		 */
+
+	}
+
+}
