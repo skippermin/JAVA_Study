c92526299ecb0ab40457c85216a23b4b6ed19034
jangminwoo <skipper44@naver.com>
2025-11-03 23:39:55 +0900

    이것도 깃허브 업로드 가능?

---
+++ First/src/chapter2/Agetest.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class Agetest {
+	public static void main(String[] args) {
+		int age;
+		age = 28;
+		System.out.println(age);
+	}
+
+}
---
+++ First/src/chapter2/Variable1.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int level; // 정수형 변수 level 을 선
+		level = 10; // level 변수에 값 10을 선
+		System.out.println(level); // level 값을 출
+	}
+
+}
